37e3d765d9037811abb465e1ac2744ab39ed66e7
Klaus0110 <114591698+Klaus0110@users.noreply.github.com>
2022-10-03 22:42:31 +0530

    BinarySearch

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,65 +1,43 @@
 import java.util.Scanner;
 
-/*
- * Java Program to implement binary search without using recursion
- */
 public class BinarySearch {
-
     public static void main(String[] args) {
-
-        Scanner commandReader = new Scanner(System.in);
-        System.out.println("Welcome to Java Program to perform 
-                               binary search on int array");
-        System.out.println("Enter total number of elements : ");
-        int length = commandReader.nextInt();
-        int[] input = new int[length];
-
-        System.out.printf("Enter %d integers %n", length);
-        for (int i = 0; i < length; i++) {
-            input[i] = commandReader.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[5];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i< a.length; i++){
+            a[i] = sc.nextInt();
         }
-
-        System.out.println("Please enter number to be searched in array 
-                                    (sorted order)");
-        int key = commandReader.nextInt();
-
-        int index = performBinarySearch(input, key);
-
-        if (index == -1) {
-            System.out.printf("Sorry, %d is not found in array %n", key);
-        } else {
-            System.out.printf("%d is found in array at index %d %n", key,
-                                                         index);
-        }
-
-        commandReader.close();
-
+        System.out.println("Enter the value to be searched: ");
+        int x = sc.nextInt();
+        sc.close();
+        long start = System.nanoTime();
+        binary(a, x);
+        long end = System.nanoTime();
+        long execution = end - start;
+        double seconds = (double) execution/ 1000000000.0;
+        System.out.println("Execution time: " + seconds + " seconds");
     }
-
-    /**
-     * Java method to perform binary search. It accept an integer array and a
-     * number and return the index of number in the array. If number doesn't
-     * exists in array then it return -1
-     *
-     * @param input
-     * @param number
-     * @return index of given number in array or -1 if not found
-     */
-    public static int performBinarySearch(int[] input, int number) {
-        int low = 0;
-        int high = input.length - 1;
-
-        while (high >= low) {
-            int middle = (low + high) / 2;
-            if (input[middle] == number) {
-                return middle;
-            } else if (input[middle] < number) {
-                low = middle + 1;
-            } else if (input[middle] > number) {
-                high = middle - 1;
+    public static void binary(int a[], int x){
+        int lo = 0;
+        int hi = a.length-1;
+        while (hi - lo>1){
+            int mid = (hi+lo)/2;
+            if (a[mid]<x){
+                lo = mid+1;
+            }
+            else {
+                hi = mid;
             }
         }
-        return -1;
+        if (a[lo] == x){
+            System.out.println("Found At Index "+lo);
+        }
+        else if (a[hi] == x) {
+            System.out.println("Found At Index "+hi);
+        }
+        else {
+            System.out.println("Not Found");
+        }
     }
-
 }
