50fd8d835752f01a36e1970557530165c8e21052
AkankshaSengar13 <115158207+AkankshaSengar13@users.noreply.github.com>
2022-10-06 21:34:16 +0530

    Add files via upload

---
+++ Switch1.java
@@ -0,0 +1,24 @@
+public class Switch1 {
+    public static void main(String args[]) {
+           // char grade = args[0].charAt(0);
+           char grade = 'C';
+     
+           switch(grade) {
+              case 'A' :
+                 System.out.println("Excellent!"); 
+                 break;
+              case 'B' :
+              case 'C' :
+                 System.out.println("Well done");
+                 break;
+              case 'D' :
+                 System.out.println("You passed");
+              case 'F' :
+                 System.out.println("Better try again");
+                 break;
+              default :
+                 System.out.println("Invalid grade");
+           }
+           System.out.println("Your grade is " + grade);
+        }
+     }
